6e948dc63c702837e46124cd3952abcbf3c2355c
Jarrod Alexander <jarrod.alexander@gmail.com>
2018-01-07 15:21:40 -0600

    del

--- java/Raytracer with shadows and IO/Src/GlobalAmbient.java
+++
@@ -1,4 +0,0 @@
-public abstract class GlobalAmbient
-{
-  public static RGBColor color = new RGBColor(0.2, 0.2, 0.2);
-}
--- java/Raytracer with shadows and IO/Src/TestUtilSolveQuadratic.java
+++
@@ -1,9 +0,0 @@
-public class TestUtilSolveQuadratic
-{
-  public static void main(String[] arg)
-  {
-    DoubleList dl = Util.solveQuadraticPositive(1, 1, -2);
-	 
-	 dl.dump();
-  }
-}
