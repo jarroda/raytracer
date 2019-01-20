921241a3d148d5de6b66a564d24992a8460e5883
Jarrod Alexander <jarrod.alexander@gmail.com>
2019-01-20 17:11:17 -0600

    Adding console test program

---
+++ src/Tracer.Console/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using Tracer.ImageSharp;
+
+namespace Tracer.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AmbientLight.Color = Vector.CreateVector3(0.2, 0.2, 0.2);
+
+            var sphere1 = new Sphere(Vector.CreateVector3(0.8, 0.0, 0), Vector.CreateVector3(1, 1, 1), 150)
+            {
+                Name = "Yellow Sphere",
+            };
+            sphere1.Model.Translate(-0.75, 0, 0);	
+            var sphere2 = new Sphere(Vector.CreateVector3(0.0, 0, 0.8), Vector.CreateVector3(1, 1, 1), 150)
+            {
+                Name = "Red Sphere",
+            };
+            sphere2.Model.Translate(0.75, 0, 0);
+
+            var scene = new Scene
+            {
+                Center = Vector.CreateVector3(0, 0, 0),
+                Eye = Vector.CreateVector3(0, 0, 5),
+                Up = Vector.CreateVector3(0, 1, 0),
+                ViewingDistance = 5,
+                Lights = new List<Light>
+                {
+                    new PointLight { Location = Vector.CreateVector3(5, 3, 5), Color = Vector.CreateVector3(1, 1, 1) },
+                },
+                Objects = new List<Traceable> { sphere1, sphere2 },
+            };
+
+            using (var outStream = File.OpenWrite("quack.png"))
+            using (var image = new Image<Rgba32>(1000, 1000))
+            {
+                scene.RenderTo(image);
+
+                image.SaveAsPng(outStream);
+            }
+        }
+    }
+}
--- src/Tracer.ImageSharp/ImageSharpRenderer.cs
+++ src/Tracer.ImageSharp/ImageSharpRenderer.cs
@@ -5,13 +5,36 @@ using System.IO;
 using System.Linq;
 using MathNet.Numerics.LinearAlgebra;
 using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 
 namespace Tracer.ImageSharp
 {
 
     public static class ImageSharpRenderer
     {
-       
+        public static void RenderTo(this Scene scene, Image<Rgba32> image)
+        {
+            //             Ray ray;
+            // Vector<double> viewingPoint;
+            // Vector<double> worldPoint;
+            // Vector<double> color;
+            // PointF viewportPoint;
+            // PointF windowPoint = new PointF(0, 0);
+            var v = new RectangleF(0, 0, image.Width, image.Height);
+            // var v = new RectangleF(0, 0, 500, 500);
+	        //var w = new RectangleF(-2.0f, 1.5f, 4.0f, 3.0f);
+            var w = new RectangleF(-2.0f, 2.0f, 4.0f, 4.0f);
+            var v2w = new ViewportToWindowTransform(w, v);
+
+            var viewing = Viewing.ViewingTransform(
+                scene.Eye,
+                scene.Center,
+                scene.Up
+            );
+
+            image.RenderTo(v2w, scene.ViewingDistance, viewing, scene.Eye,
+                scene.Objects, scene.Lights);
+        }
 
         public static void RenderTo(this Image<Rgba32> image, IViewportToWindowTransform v2w, double viewDistance, Matrix<double> viewing, Vector<double> eye, IEnumerable<Traceable> objects, IEnumerable<Light> lights)
         {
